6ca04023bbae627059fb9836ab4c18c20df7473f
Silver Okonkwo <silvernnaokonkwo@gmail.com>
2023-06-25 18:54:35 +0100

    fixed exec

--- frontend/src/components/config.js
+++ frontend/src/components/config.js
@@ -1,5 +1,5 @@
-// const API_URL = 'https://scissorbackend.onrender.com';
-const API_URL = 'http://localhost:4001';
+const API_URL = 'https://scissorbackend.onrender.com';
+// const API_URL = 'http://localhost:4001';
 
 
 export default API_URL;
